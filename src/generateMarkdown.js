b89c0e431b358bb5b43b2462f21d2461db774d69
Daniel Merkin <dan.merkin@gmail.com>
2022-08-24 13:51:18 -0400

    styling with bootstrap with dynamic HTML

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,5 +1,34 @@
 const generateMarkdown = (data) => {
-  return `testing ${data.name}`;
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Employee Team Profile Generator</title>
+  </head>
+  <body>
+   <header class="jumbotron text-center bg-danger text-white">
+    <h1>Our Team</h1>
+   </header>
+   <main>
+   <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${data.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${data.position}</h6>
+        <p class="card-text">ID: ${data.id}</p>
+        <a href="${data.email}" class="card-link">${data.email}</a>
+        <p class="card-text">${data.number}</p>
+      </div>
+    </div>
+
+   </main>
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  </html>`;
 };
 
 module.exports = generateMarkdown;
